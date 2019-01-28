6fd7cb75249df1a37b8ed895d0568567edd19ffd
Thomas <thomasweems3@gmail.com>
2019-01-27 18:49:56 -0600

    add some form validation

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
-function navFunction() {
-    var x = document.getElementById("navLinks");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  }
-
-  document.getElementById("uploadBtn").onchange = function () {
+// show file string if file is selected
+document.getElementById("uploadBtn").onchange = function () {
     document.getElementById("uploadFile").value = this.value;
 };
+
+// Form message alert if showAlert function is called
+function showAlert(msg) {
+  const div = document.createElement('div');
+  div.className = `alert alert-danger w-75`;
+  div.appendChild(document.createTextNode(msg));
+  const contact = document.querySelector('.contact')
+  const form = document.querySelector('#contactForm')
+  contact.insertBefore(div, form);
+  setTimeout(() => document.querySelector('.alert').remove(), 5000);
+}
+
+
+// Form submit listener
+document.querySelector('#contactForm').addEventListener('submit', (e) => {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  if(name === '') {
+    e.preventDefault();
+    nameInput.className = `form-control my-3 border-danger`;
+    showAlert('Please Fill In Required Fields');
+
+    return;
+  }
+  if(email === '') {
+    e.preventDefault();
+    emailInput.className = `form-control my-3 border-danger`;
+    showAlert('Please Fill In Required Fields');
+
+    return;
+  }
+});
+
+// if name input changes remove red border
+document.getElementById('name').addEventListener('input', (e) => {
+  const nameInput = document.getElementById('name');
+  // apply the following class
+  nameInput.className = `form-control my-3`;
+})
+
+// if email input changes remove red border
+document.getElementById('email').addEventListener('input', (e) => {
+  const emailInput = document.getElementById('email');
+  // apply the following class
+  emailInput.className = `form-control my-3`;
+})
